090ff36b84cceaecd6a9a387dec428d2007cbebd
GhosT <d.chet@bk.ru>
2023-06-27 21:38:13 +0300

    finish categories types sizes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function Categories() {
   const [activeCategoryIndex, setActiveCategoryIndex] = useState(0);
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   const onClickCategory = (index) => {
     setActiveCategoryIndex(index);
@@ -9,24 +10,11 @@ function Categories() {
   return (
     <div className="categories">
       <ul>
-        <li onClick={() => onClickCategory(0)} className={activeCategoryIndex === 0 ? 'active' : ''}>
-          Все
-        </li>
-        <li onClick={() => onClickCategory(1)} className={activeCategoryIndex === 1 ? 'active' : ''}>
-          Мясные
-        </li>
-        <li onClick={() => onClickCategory(2)} className={activeCategoryIndex === 2 ? 'active' : ''}>
-          Вегетарианская
-        </li>
-        <li onClick={() => onClickCategory(3)} className={activeCategoryIndex === 3 ? 'active' : ''}>
-          Гриль
-        </li>
-        <li onClick={() => onClickCategory(4)} className={activeCategoryIndex === 4 ? 'active' : ''}>
-          Острые
-        </li>
-        <li onClick={() => onClickCategory(5)} className={activeCategoryIndex === 5 ? 'active' : ''}>
-          Закрытые
-        </li>
+        {categories.map((value, index) => (
+          <li onClick={() => onClickCategory(index)} className={activeCategoryIndex === index ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
