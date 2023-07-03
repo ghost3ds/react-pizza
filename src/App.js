293a2152049f9a8112e624f1919224ec2bb02650
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2023-07-03 17:10:21 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './scss/app.scss';
 
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
