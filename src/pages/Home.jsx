b91f5fe0efbeeeb8922c54ebd2e4218755870214
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2023-07-05 21:06:19 +0300

    grid cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,10 +17,11 @@ const Home = () => {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -29,7 +30,7 @@ const Home = () => {
       <div className="content__items">
         {isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />) : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 export default Home;
