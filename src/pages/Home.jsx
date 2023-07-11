cbe2a283b7c1a254888976727e972b4eaf79315f
GhosT <d.chet@bk.ru>
2023-07-11 21:03:28 +0300

    pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,28 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'rating',
   });
   const [orderType, setOrderType] = useState('asc');
+
   useEffect(() => {
     setIsLoading(true);
+
+    const search = searchValue ? `&search=${searchValue}` : '';
+
     fetch(
-      `https://636f5291f2ed5cb047daa480.mockapi.io/items?${
+      `https://636f5291f2ed5cb047daa480.mockapi.io/items?page=${currentPage}&limit=4&${
         categoryId > 0 ? `category=${categoryId}` : ''
-      }&sortBy=${sortType.sortProperty}&order=${orderType}`,
+      }&sortBy=${sortType.sortProperty}&order=${orderType}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -28,7 +34,7 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, orderType]);
+  }, [categoryId, sortType, orderType, searchValue, currentPage]);
 
   return (
     <div className="container">
@@ -41,16 +47,9 @@ const Home = ({ searchValue }) => {
       <div className="content__items">
         {isLoading
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items
-              .filter((obj) => {
-                if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-                  return true;
-                }
-
-                return false;
-              })
-              .map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
