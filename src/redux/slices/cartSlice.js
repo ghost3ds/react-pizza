b96b07569e71ab9b33fefff5e8f5fc6532f148ce
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2023-07-19 20:52:40 +0300

    redux cart 2

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,12 +9,6 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    // addItem(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => {
-    //     return obj.price + sum;
-    //   }, 0);
-    // },
     addItem(state, action) {
       const findItem = state.items.find((obj) => obj.id == action.payload.id);
 
@@ -30,14 +24,29 @@ const cartSlice = createSlice({
         return obj.price * obj.count + sum;
       }, 0);
     },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id == action.payload);
+      if (findItem) {
+        findItem.count--;
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id != action.payload);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
     },
     clearItems(state) {
       state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { addItem, removeItem, clearItems } = cartSlice.actions;
+export const { addItem, removeItem, clearItems, minusItem } = cartSlice.actions;
 export default cartSlice.reducer;
