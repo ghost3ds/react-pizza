7e6b99badff3690c236c95148f305586fdce2f7e
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2023-08-01 21:43:38 +0300

    useParams

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const FullPizza = () => {
+  const [pizza, setPizza] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://636f5291f2ed5cb047daa480.mockapi.io/items/' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы...');
+      }
+    }
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return 'Загрузка...';
+  }
+
+  return (
+    <div className="container pizza-block">
+      <img className="pizza-block__image" src={pizza.imageUrl} alt="pizza" />
+      <h2 className="pizza-block__title">{pizza.title}</h2>
+      <h4 className="pizza-block__price">{pizza.price}₽</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ import Sort, { list } from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
-import { SearchContext } from '../App';
 import {
   setCategoryId,
   setCurrentPage,
